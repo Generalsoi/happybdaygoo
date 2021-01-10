b75b2369b252c77d35b5b1c1a7f6d88e9a5e501a
Generalsoi <ikhinobelesuccess@gmail.com>
2021-01-10 18:22:26 +0100

    updates to the site

--- script.js
+++ script.js
@@ -2,5 +2,24 @@ var intro = new Vue({
     el: "#intro",
     data: {
         name: "GOO",
+        password: ""
+    },
+    methods: {
+        onSubmit(){
+            if (this.password === "goo"){
+                window.location.href="birthday.html"
+            } else{
+                alert("Wrong password")
+            }
+        }
+    }
+})
+
+var birthdayContent = new Vue({
+    el: "#birthday-content",
+    data: {
+        message: "HAPPY BIRTHDAY SWEETHEART",
+        image1: "./images/goo1.jpg",
+        image2: "./images/goo2.jpg"
     }
 })
